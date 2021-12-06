b8d3c01eafd4c3b21f0307fa28a4270fbb2ffe5d
StoneMooseGames <mika.ahlsten@stonemoose.com>
2021-12-06 15:54:01 +0200

    backend paths added

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def userindex(request):
+    return render(request, 'registration/usersindex.html')
+    
+def register(request):
+    return render(request, 'registration/register.html')
+
+
+
